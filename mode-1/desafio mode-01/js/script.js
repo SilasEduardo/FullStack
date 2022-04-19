8c99bd4d94602dc38de0b39e7e06dad7dc48aba7
SilasEdu001 <silasandrade944@gmail.com>
2022-04-19 10:33:25 -0300

    desafio

--- mode-1/desafio mode-01/js/script.js
+++ mode-1/desafio mode-01/js/script.js
@@ -1,42 +1,155 @@
-// https://randomuser.me/api/?seed=javascript&results=100&nat=BR&noinfo
-let usersInfo = [];
-let inpuntName = 'Araújo'
-let male = null;
-let famelr = null
-let age = null
-let avarageAge = null;
-let input = document.querySelector('#inpunt').focus()
+let namePeople= null;
+let peopleFound = null;
+let peopleFilter = null;
+let peopleRander = '';
+let contF = 0;
+let contM = 0;
+let agePeople = 0;
+let btn =  document.getElementById("btn")
+let masc = document.querySelector('#m')
+let fame = document.querySelector('#f')
+
+let peopleFoundHtml = document.querySelector('#peopleFoundHtml')
+let agePeoples = document.querySelector('#age')
+let inputName = document.querySelector("#inpunt")
+let numberPeople = document.querySelector("#number")
+let averageHTML = document.querySelector("#average")
+
+let statisticPeople = document.querySelector('#statistic')
+
+
+let avatar = null;
+let statistics = [];
 
 window.addEventListener('load', ()=>{
-    fetchUsers()
-    // nameFilter()
+  
+  doFachAsync()
+  
 })
 
-async function fetchUsers() {
-    const res = await fetch('https://randomuser.me/api/?seed=javascript&results=100&nat=BR&noinfo')
-    const json = await res.json()
-  
-    usersInfo = json.results.map(user => {
-  
-      const { name, picture, dob, gender } = user
-  
-      return {
-        name: `${name.first} ${name.last}`,
-        picture: picture.thumbnail,
-        age: dob.age,
-        gender
-      }
-    })
-   console.log(usersInfo.name)
 
+
+async function doFachAsync(){
+  const res = await fetch('https://randomuser.me/api/?seed=javascript&results=100&nat=BR&noinfo')
+  const data = await res.json()
+
+  doMAp = ()=>{
+    peopleFound = data.results.map(person =>{
+        return {
+            lastName: person.name.last,
+            firstName: person.name.first,
+            avatar: person.picture.large,
+            age: person.dob.age, 
+            gender: person.gender
+        }
+    })
+  }
+doMAp()
 }
 
-// nameFilter = () =>{
-//     const foundeName = userFound.filter(
-//        person =>{
-//         return person.name.lest === 'Araújo';
-//        } 
-//     )
 
-//     console.log(foundeName);
-// }
+
+
+inputName.addEventListener('keyup', handleTyping);
+let cont = 0;
+function handleTyping(event) {
+  //Se a tecla digitada for o Enter do teclado
+  if(event.key === 'Enter' && event.target.value.trim() !== ''){
+    namePeople = event.target.value
+    doFilter = () =>{
+      peopleFilter = peopleFound.filter(
+         person =>{
+          return person.lastName === namePeople;
+         }
+      )
+
+      function renderPeopleList() {
+        let countriesHTML = '<div class="peoples">';
+        peopleFilter.forEach(person => {
+          const {
+            lastName: lastName,
+            firstName: firstName,
+            avatar: avatar,
+            age: age
+          } = person;
+          "<div>"
+          const countryHTML = `
+            <div class = 'people'>
+              <div>
+              <img src=${avatar}>
+              </div>
+              <div>
+                <p>${firstName}</p>
+              </div>
+              <div>
+              <div>
+              <p>${lastName}</p>
+              </div>
+              </div>
+              <div>
+              <p>${age}
+              </div>
+            </div>
+
+          `;
+          peopleRander += countryHTML;
+
+          numberPeople.innerHTML = peopleFilter.length;
+
+        });
+        countriesHTML += '</div>';
+        peopleFoundHtml.innerHTML += peopleRander
+        peopleRander = '';
+
+
+
+
+        const list = peopleFoundHtml.classList;
+        list.add("people");
+        const list2 = peopleFoundHtml.classList;
+        list2 .add("peoples");
+        
+      }
+      peopleFoundHtml.innerHTML = '';
+      renderPeopleList()
+      const renderStatisticList = () => {
+        for(i of peopleFilter){
+          agePeople += i.age;
+          if(i.gender == 'female'){
+            contF ++
+          }else{
+            contM ++
+          }
+        } 
+
+        averageHTML.innerHTML = agePeople / peopleFilter.length.toFixed(2);
+        agePeoples.innerHTML = agePeople
+        masc.innerHTML = contM
+        fame.innerHTML = contF
+          
+      }
+      renderStatisticList()
+      }
+      
+    contF = 0;
+    contM = 0;
+    agePeople =0;
+
+    doFilter()
+  }
+  
+ 
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
